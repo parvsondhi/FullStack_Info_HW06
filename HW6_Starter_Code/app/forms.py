2ee42f6c23b4618448ce747bd4243a08c1adab73
nolanchao <nolan_chao@mba.berkeley.edu>
2018-03-08 00:46:53 -0800

    Add files via upload

--- HW6_Starter_Code/app/forms.py
+++ HW6_Starter_Code/app/forms.py
@@ -5,8 +5,18 @@ from wtforms.validators import DataRequired
 
 class CustomerForm(Form):
     company = StringField('company', validators=[DataRequired()])
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
     email = EmailField('email', validators=[DataRequired()])
     # Add additional Address fields here
+    street_address  = StringField('street_address', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state  = StringField('state', validators=[DataRequired()])
+    country  = StringField('country', validators=[DataRequired()])
+    zip_code = StringField('zip_code', validators=[DataRequired()])
 
 class OrderForm(Form):
-    # Add order input form fields here
+	# Add order input form fields here
+	name_of_part = StringField('name_of_part', validators=[DataRequired()])
+	manufacturer_of_part = StringField('manufacturer_of_part', validators=[DataRequired()])
