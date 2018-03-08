77838c6ceef1c8a033b6a4439a03d7ae1d8ad38c
Ching-Yi Lin <icequ60914@gmail.com>
2018-03-08 15:45:03 -0800

    complete hw06

--- HW6_Starter_Code/app/views.py
+++ HW6_Starter_Code/app/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, request
 from app import app, models, db
-from .forms import CustomerForm
+from .forms import *
 # Access the models file to use SQL functions
+from .models import *
 
 
 @app.route('/')
@@ -14,18 +15,40 @@ def create_customer():
     if form.validate_on_submit():
         # Get data from the form
         # Send data from form to Database
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        company = form.company.data
+        email = form.email.data
+        phone = form.phone.data
+        # insert_customer(first_name, last_name, company, email, phone)
+
+        street = form.street.data
+        city = form.city.data
+        state = form.state.data
+        country = form.country.data
+        zip_code = form.zip_code.data
+        insert_customer(first_name, last_name, company, email, phone, street, city, state, country, zip_code)
+        # insert_address(street, city, state, country, zip_code)
         return redirect('/customers')
     return render_template('customer.html', form=form)
 
 @app.route('/customers')
 def display_customer():
     # Retreive data from database to display
+    customers = retrieve_customers()
+    orders = retrieve_orders()
     return render_template('home.html',
-                            customers=customers)
+                            customers=customers, orders=orders)
 
 @app.route('/create_order/<value>', methods=['GET', 'POST'])
 def create_order(value):
+    # orderForm = OrderForm()
+    form = OrderForm()
+    if form.validate_on_submit():
         # Get data from the form
         # Send data from form to Database
+        name_of_part = form.name_of_part.data
+        manufacturer_of_part = form.manufacturer_of_part.data
+        insert_order(value, name_of_part, manufacturer_of_part)
         return redirect('/customers')
-    return render_template('order.html', form=orderForm)
+    return render_template('order.html', form=form)
