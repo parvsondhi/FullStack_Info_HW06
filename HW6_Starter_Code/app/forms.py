a8ed153482eb9dd498aced3de1121cd68eef8348
Xinhe Ren <jim.x.ren@berkeley.edu>
2018-03-08 02:22:21 -0800

    Finished hw6

--- HW6_Starter_Code/app/forms.py
+++ HW6_Starter_Code/app/forms.py
@@ -4,9 +4,19 @@ from flask_wtf.html5 import EmailField
 from wtforms.validators import DataRequired
 
 class CustomerForm(Form):
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
     company = StringField('company', validators=[DataRequired()])
     email = EmailField('email', validators=[DataRequired()])
-    # Add additional Address fields here
+    phone = StringField('phone', validators=[DataRequired()])
+
+    street = StringField('street', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state = StringField('state', validators=[DataRequired()])
+    country = StringField('country', validators=[DataRequired()])
+    zipcode = IntegerField('zipcode', validators=[DataRequired()])
 
 class OrderForm(Form):
     # Add order input form fields here
+    part_name = StringField('part_name', validators=[DataRequired()])
+    manufacturer = StringField('manufacturer', validators=[DataRequired()])
