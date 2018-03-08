f5535b30a515ca141e806c2a57114c80b6075240
Devin Huang <devin_huang@berkeley.edu>
2018-03-08 15:55:33 -0800

    HW6

--- HW6_Starter_Code/schema.sql
+++ HW6_Starter_Code/schema.sql
@@ -1,2 +1,39 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
+drop table if exists customers;
+create table customers (
+	customer_id integer primary key,
+	first_name text not null,
+	last_name text not null,
+	company text not null,
+	email text not null,
+	phone integer not null
+);
+
+drop table if exists address;
+create table address (
+	id integer primary key,
+	street_address text not null,
+	city text not null,
+	state text not null,
+	country text not null,
+	zip_code text not null
+);
+
+drop table if exists orders;
+create table orders (
+	order_id integer primary key,
+	name_of_part text not null,
+	manufacturer_of_part text not null,
+	customer_id integer not null,
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+drop table if exists orders_customers;
+create table orders_customers (
+	orders_customers_id integer primary key,
+	order_id integer not null,
+	customer_id integer not null,
+	FOREIGN KEY(order_id) REFERENCES orders(order_id),
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
