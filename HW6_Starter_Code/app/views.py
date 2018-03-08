41c05aaa4aea413ae46feabb93b520a1d8702a22
daphnejong <daphne.jong@berkeley.edu>
2018-03-07 23:09:25 -0800

    changes to hw

--- HW6_Starter_Code/app/views.py
+++ HW6_Starter_Code/app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, request
 from app import app, models, db
 from .forms import CustomerForm
+from .forms import OrderForm
+from .models import *
 # Access the models file to use SQL functions
 
 
@@ -14,18 +16,39 @@ def create_customer():
     if form.validate_on_submit():
         # Get data from the form
         # Send data from form to Database
+        firstname = form.firstname.data
+        lastname = form.lastname.data
+        company = form.company.data
+        email = form.email.data
+        phone = form.phone.data
+        address = form.address.data
+        city = form.city.data
+        state = form.state.data
+        country = form.country.data
+        zipcode = form.zipcode.data
+        customer_id = insert_customer(firstname,lastname,company,email,phone)
+        insert_address(address,city,state,country,zipcode,customer_id)
         return redirect('/customers')
     return render_template('customer.html', form=form)
 
 @app.route('/customers')
 def display_customer():
     # Retreive data from database to display
+    customers = retrieve_customers()
+    orders = retrieve_orders()
     return render_template('home.html',
-                            customers=customers)
+                            customers=customers,
+                            orders=orders)
 
 @app.route('/create_order/<value>', methods=['GET', 'POST'])
 def create_order(value):
+    form = OrderForm()
+    if form.validate_on_submit():
         # Get data from the form
         # Send data from form to Database
+        name_of_part = form.name_of_part.data
+        manufacturer = form.manufacturer.data
+        order_id = insert_order(name_of_part, manufacturer, value)
+        insert_customers_order(value, order_id)
         return redirect('/customers')
-    return render_template('order.html', form=orderForm)
+    return render_template('order.html', form=form)
