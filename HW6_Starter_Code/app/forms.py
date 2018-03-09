16c63206af0493882236199d916c01f98fa1669b
peterrowland <peter@airbears2-10-142-151-150.airbears2.1918.berkeley.edu>
2018-03-08 16:37:38 -0800

    had to reset to my own branch

--- HW6_Starter_Code/app/forms.py
+++ HW6_Starter_Code/app/forms.py
@@ -6,7 +6,17 @@ from wtforms.validators import DataRequired
 class CustomerForm(Form):
     company = StringField('company', validators=[DataRequired()])
     email = EmailField('email', validators=[DataRequired()])
-    # Add additional Address fields here
+    fname = StringField('fname', validators=[DataRequired()])
+    lname = StringField('lname', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
+
+class AddressForm(Form):
+    street_address = StringField('street_address', validators=[DataRequired()])
+    city = StringField('city', validators=[DataRequired()])
+    state = StringField('state', validators=[DataRequired()])
+    country = StringField('country', validators=[DataRequired()])
+    zip_code = IntegerField('zip_code', validators=[DataRequired()])
 
 class OrderForm(Form):
-    # Add order input form fields here
+    part_name = StringField('part_name', validators=[DataRequired()])
+    manufacturer = StringField('manufacturer', validators=[DataRequired()])
--- HW6_Starter_Code/schema.sql
+++ HW6_Starter_Code/schema.sql
@@ -1,2 +1,44 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
+
+drop table if exists customers;
+create table customers(
+  customer_id integer primary key,
+  company text not null,
+  email text not null,
+  fname text not null,
+  lname text not null,
+  phone text not null);
+
+drop table if exists addressess;
+create table addressess(
+  id integer primary key,
+  customer_id integer,
+  street_address text not null,
+  city text not null,
+  state text not null,
+  country text not null,
+  zip_code integer not null);
+
+drop table if exists orders;
+create table orders(
+  order_id integer primary key,
+  part_name text not null,
+  manufacturer text not null);
+
+drop table if exists customers_orders;
+create table customers_orders(
+  id integer primary key,
+  customer_id integer not null,
+  order_id integer not null);
+
+-- Dummy Data for testing
+
+INSERT into customers (company, email, fname, lname, phone)
+VALUES ("SPECTRE","hblofeld@spectre.net","Hans","Blofeld","510-393-8888");
+
+INSERT into addressess (street_address, city, state, country, zip_code)
+VALUES ("Unknown","Undisclosed","XX","Void","00000");
+
+INSERT into orders (part_name, manufacturer)
+VALUES ("fidget spinner", "Fidgies");
