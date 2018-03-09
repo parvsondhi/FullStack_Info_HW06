a0a203ff92c5bd1baccac072320c7e96e07aca4f
DimitriosHytiroglou <DimitriosHytiroglou@users.noreply.github.com>
2018-03-08 16:30:10 -0800

    Finished HW

--- HW6_Starter_Code/app/forms.py
+++ HW6_Starter_Code/app/forms.py
@@ -1,12 +1,25 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, IntegerField
+# from wtforms_components import PhoneNumberField
 from flask_wtf.html5 import EmailField
 from wtforms.validators import DataRequired
 
 class CustomerForm(Form):
-    company = StringField('company', validators=[DataRequired()])
-    email = EmailField('email', validators=[DataRequired()])
-    # Add additional Address fields here
+	firstName = StringField('firstName', validators=[DataRequired()])
+	lastName = StringField('lastName', validators=[DataRequired()])
+	company = StringField('company', validators=[DataRequired()])
+	email = EmailField('email', validators=[DataRequired()])
+	telephone = StringField('telephone', validators=[DataRequired()])
 
-class OrderForm(Form):
-    # Add order input form fields here
+	# Add additional Address fields here
+	streetAddress = StringField('streetAddress', validators=[DataRequired()])
+	state = StringField('state', validators=[DataRequired()])
+	country = StringField('country', validators=[DataRequired()])
+	zipCode = StringField('zipCode', validators=[DataRequired()])
+
+
+class orderForm(Form):
+	# Add order input form fields here
+	nameOfPart = StringField('nameOfPart', validators=[DataRequired()])
+	manufacturerOfPart = StringField('manufacturerOfPart', validators=[DataRequired()])
+	
--- HW6_Starter_Code/schema.sql
+++ HW6_Starter_Code/schema.sql
@@ -1,2 +1,48 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
+
+drop table if exists customers;
+
+CREATE TABLE customers(
+	customer_id integer PRIMARY KEY,
+	first_name text not null,
+	last_name text not null,
+	company text not null,
+	email text not null,
+	phone char not null 
+);
+
+
+
+drop table if exists addresses;
+
+CREATE TABLE addresses(
+	id integer,
+	street_address text not null,
+	state_ text not null,
+	country text not null,
+	zip_code text not null,
+    FOREIGN KEY (id) REFERENCES customers(customer_id)
+	CONSTRAINT PK_Address PRIMARY KEY (sreet_address, state_, country, zip_code))
+);
+
+
+drop table if exists orders;
+	
+CREATE TABLE orders(
+	order_id integer PRIMARY KEY,
+	name_of_part text not null,
+	manufacturer_of_part text not null,
+	);
+
+
+
+DROP TABLE if exists order_match;
+
+CREATE TABLE order_match(
+	id integer PRIMARY KEY,
+	order_id integer,
+	customer_id integer,	
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+	);
