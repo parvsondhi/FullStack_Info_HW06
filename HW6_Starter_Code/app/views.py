bb94495d863a957b7ff5ab9aacd4cd98a1100380
Eric Huynh <erickhuynh@GMD-MACBOOK-EH1.local>
2018-03-14 22:11:05 -0700

    eric

--- HW6_Starter_Code/app/views.py
+++ HW6_Starter_Code/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, request
 from app import app, models, db
-from .forms import CustomerForm
+from .forms import CustomerForm, OrderForm
+from .models import *
 # Access the models file to use SQL functions
 
 
@@ -14,18 +15,39 @@ def create_customer():
     if form.validate_on_submit():
         # Get data from the form
         # Send data from form to Database
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        company = form.company.data
+        email = form.email.data
+        phone = form.phone.data
+        street_address = form.street_address.data
+        city = form.city.data
+        state = form.state.data
+        country = form.country.data
+        zip_code = form.zip_code.data
+        customer_id = insert_data(first_name, last_name, company, email, phone)
+        insert_address(street_address, city, state, country, zip_code, customer_id)
         return redirect('/customers')
     return render_template('customer.html', form=form)
 
 @app.route('/customers')
 def display_customer():
     # Retreive data from database to display
+    customers = retrieve_customers()
+    orders = retrieve_orders()
     return render_template('home.html',
-                            customers=customers)
+                            customers=customers,
+                            orders=orders)
 
 @app.route('/create_order/<value>', methods=['GET', 'POST'])
 def create_order(value):
         # Get data from the form
         # Send data from form to Database
+        form = OrderForm()
+        if form.validate_on_submit():
+                customer_id = value
+                name_of_part = form.name_of_part.data
+                manufacturer_of_part = form.manufacturer_of_part.data
+                insert_order(name_of_part,manufacturer_of_part,customer_id)
         return redirect('/customers')
-    return render_template('order.html', form=orderForm)
+        return render_template('order.html', form=form)
--- HW6_Starter_Code/schema.sql
+++ HW6_Starter_Code/schema.sql
@@ -1,2 +1,32 @@
 -- Insert code to create Database Schema
 -- This will create your .db database file for use
+drop table if exists customers;
+create table customers (
+	customer_id integer primary key,
+	first_name text not null,
+	last_name text not null,
+	email text not null,
+	company text not null,
+	phone integer not null
+	);
+
+drop table if exists orders;
+create table orders (
+	order_id integer primary key,
+	name_of_part text not null,
+	manufacturer_of_part text not null,
+	customer_id text not null,
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+	);
+
+drop table if exists address;
+create table address (
+	address_id integer primary key,
+	street_address text not null,
+	city text not null,
+	state text not null,
+	country text not null,
+	zip_code integer not null,
+	customer_id integer not null,
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+	);
